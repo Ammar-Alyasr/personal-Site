c69bcc855dcfddc971cd009ae04b87ae839bbea5
Ammar-Alyasr <you@example.com>
2017-04-02 11:39:52 +0300

    marginleri dueltme

--- js.js
+++ js.js
@@ -1,3 +1,15 @@
 /**
+ * smoothScroll
  * Created by yapı123 on 17.03.2017.
  */
+
+$(function () {
+    $("icerikSayifasi").height($(window).height())
+    $("ikinciIcerik").height($(window).height())
+
+    smoothScroll.init({
+        speed : 2000
+    });
+
+
+})
